194fe087a51f71fa3c507ebb7e579a0503e323d5
adira.sayidina <adira.sayidina@ui.ac.id>
2022-04-09 13:11:43 +0700

    Reg

---
+++ src/main/java/id/ac/ui/cs/advprog/frontend/UserAccDTO.java
@@ -0,0 +1,51 @@
+package id.ac.ui.cs.advprog.frontend;
+
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.*;
+
+@NoArgsConstructor
+public class UserAccDTO {
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String username;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
--- src/main/java/id/ac/ui/cs/advprog/frontend/WebController.java
+++ src/main/java/id/ac/ui/cs/advprog/frontend/WebController.java
@@ -1,12 +1,28 @@
 package id.ac.ui.cs.advprog.frontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class WebController {
     @GetMapping("/")
     public String getHomePage() {
+
         return "home.html";
+    }
+
+    @GetMapping("/createAccount")
+    public String createUser(WebRequest request, Model model) {
+        model.addAttribute("user", new UserAccDTO());
+        return "createUser";
+    }
+
+    @PostMapping("/createAccount")
+    public String createUserPost(UserAccDTO dto, Model model) {
+        //
+        return "home";
     }
 }
