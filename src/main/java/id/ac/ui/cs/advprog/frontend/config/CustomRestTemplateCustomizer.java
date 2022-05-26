a8e18bb13e0110bbc6dc656db42e3f57e5a6ff65
adira.sayidina <adira.sayidina@ui.ac.id>
2022-05-26 20:58:19 +0700

    deploy

--- src/main/java/id/ac/ui/cs/advprog/frontend/config/CustomRestTemplateCustomizer.java
+++ src/main/java/id/ac/ui/cs/advprog/frontend/config/CustomRestTemplateCustomizer.java
@@ -7,6 +7,6 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 public class CustomRestTemplateCustomizer implements RestTemplateCustomizer {
     @Override
     public void customize(RestTemplate restTemplate) {
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8081/"));
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("https://b13-auth-gateway.herokuapp.com"));
     }
 }
